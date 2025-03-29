7da6b7750f2742205b4166a1ae6116085a52be4e
li-chx <lchx751176501@gmail.com>
2025-03-30 05:54:05 +0800

    [deploy] 从环境变量读取值

--- iwut-shu_tu-backend/config/config.js
+++ iwut-shu_tu-backend/config/config.js
@@ -1,5 +1,5 @@
-module.exports={
-    DBHOST:'127.0.0.1',
-    DBPORT:27017,
-    DBNAME:'shutupost',
-}
+module.exports = {
+    DBHOST: process.env.DBHOST || '127.0.0.1', // 默认值为 '127.0.0.1'
+    DBPORT: process.env.DBPORT || 27017,      // 默认值为 27017
+    DBNAME: process.env.DBNAME || 'shutupost' // 默认值为 'shutupost'
+};
